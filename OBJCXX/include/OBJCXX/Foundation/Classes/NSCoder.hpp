42346db2723c1433f6620ec9124d102fa0f865eb
macmade <macmade@xs-labs.com>
2015-12-25 01:21:21 +0100

    NSCoder method declarations.

--- OBJCXX/include/OBJCXX/Foundation/Classes/NSCoder.hpp
+++ OBJCXX/include/OBJCXX/Foundation/Classes/NSCoder.hpp
@@ -31,6 +31,9 @@
 #define OBJCXX_FOUNDATION_CLASSES_NS_CODER_H
 
 #include <OBJCXX/Foundation/Classes/NSObject.hpp>
+#include <OBJCXX/Foundation/Classes/NSString.hpp>
+#include <OBJCXX/Foundation/Classes/NSData.hpp>
+#include <OBJCXX/Foundation/Classes/NSSet.hpp>
 
 namespace NS
 {
@@ -41,6 +44,83 @@ namespace NS
             using Object::Object;
             
             Coder( void );
+            
+            bool allowsKeyedCoding( void );
+            bool containsValueForKey( const String & key );
+            void encodeArrayOfObjCType( const char * type, UInteger count, const void * array );
+            void encodeBool( bool boolv, const String & key );
+            void encodeBycopyObject( id anObject );
+            void encodeByrefObject( id anObject );
+            void encodeBytes( const void * byteaddr, UInteger length );
+            void encodeBytes( const uint8_t * bytesp, UInteger lenv, const String & key );
+            void encodeConditionalObject( id object );
+            void encodeConditionalObject( id objv, const String & key );
+            void encodeDataObject( const Data & data );
+            void encodeDouble( double realv, const String & key );
+            void encodeFloat( float realv, const String & key );
+            void encodeInt( int intv, const String & key );
+            void encodeInteger( Integer intv, const String & key );
+            void encodeInt32( int32_t intv, const String & key );
+            void encodeInt64( int64_t intv, const String & key );
+            void encodeNXObject( id object );
+            void encodeObject( id object );
+            void encodeObject( id objv, const String & key );
+            void encodePoint( Point point );
+            void encodePoint( Point point, const String & key );
+            void encodePropertyList( id aPropertyList );
+            void encodeRect( Rect rect );
+            void encodeRect( Rect rect, const String & key );
+            void encodeRootObject( id rootObject );
+            void encodeSize( Size size );
+            void encodeSize( Size size, const String & key );
+            void encodeValueOfObjCType( const char * type, const void * addr );
+            void encodeValuesOfObjCTypes( const char * types, ... );
+            
+            /*
+            void encodeCMTime( CMTime time, const String & key );
+            void encodeCMTimeRange( CMTimeRange timeRange, const String & key );
+            void encodeCMTimeMapping( CMTimeMapping timeMapping, const String & key );
+            */
+            
+            void            decodeArrayOfObjCType( const char * itemType, Integer count, void * array );
+            bool            decodeBoolForKey( const String & key );
+            const uint8_t * decodeBytesForKey( const String & key, UInteger * lengthp );
+            void          * decodeBytesWithReturnedLength( UInteger * lengthp );
+            Data            decodeDataObject( void );
+            double          decodeDoubleForKey( const String & key );
+            float           decodeFloatForKey( const String & key );
+            int             decodeIntForKey( const String & key );
+            Integer         decodeIntegerForKey( const String & key );
+            int32_t         decodeInt32ForKey( const String & key );
+            int64_t         decodeInt64ForKey( const String & key );
+            id              decodeNXObject( void );
+            id              decodeObject( void );
+            id              decodeObjectForKey( const String & key );
+            Point           decodePoint( void );
+            Point           decodePointForKey( const String & key );
+            Rect            decodeRect( void );
+            Rect            decodeRectForKey( const String & key );
+            Size            decodeSize( void );
+            Size            decodeSizeForKey( const String & key );
+            void            decodeValueOfObjCType( const char * type, void * data );
+            void            decodeValuesOfObjCTypes( const char * types, ... );
+            id              decodeObjectOfClass( Class aClass, const String & key );
+            id              decodeObjectOfClasses( const Set & classes, const String & key );
+            id              decodePropertyListForKey( const String & key );
+            
+            /*
+            CMTime          decodeCMTimeForKey( const String & key );
+            CMTimeRange     decodeCMTimeRangeForKey( const String & key );
+            CMTimeMapping   decodeCMTimeMappingForKey( const String & key );
+            */
+            
+            bool         requiresSecureCoding( void );
+            Set          allowedClasses( void );
+            void         setAllowedClasses( const Set & allowedClasses );
+            unsigned int systemVersion( void );
+            Integer      versionForClassName( const String & className );
+            Zone       * objectZone( void );
+            void         setObjectZone( Zone * zone );
     };
 }
 
