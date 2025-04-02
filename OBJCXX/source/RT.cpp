1c7c1e1aa2955615dbb9574aa445d258cd6df3da
macmade <macmade@xs-labs.com>
2025-04-02 16:42:26 +0200

    fix(iOS): Replaced a call to deprecated function OSAtomicCompareAndSwap32 by using std::call_once

--- OBJCXX/source/RT.cpp
+++ OBJCXX/source/RT.cpp
@@ -28,6 +28,7 @@
  */
 
 #include <OBJCXX/RT.hpp>
+#include <mutex>
 
 #ifdef _WIN32
 
@@ -39,8 +40,6 @@
 #endif
 
 #else
-
-#include <libkern/OSAtomic.h>
 
 extern "C"
 {
@@ -81,8 +80,6 @@ static void init( void )
 }
 
 #endif
-
-static int32_t inited = 0;
 
 template<>
 class XS::PIMPL::Object< OBJCXX::RT::MessageBase >::IMPL
@@ -232,115 +229,114 @@ namespace OBJCXX
         
         void Init( void )
         {
-            #ifdef _WIN32
+            static std::once_flag once;
             
-            char       * common;
-            std::string  apple;
-            HMODULE      objc;
-            HMODULE      foundation;
-
-            if( InterlockedCompareExchange( reinterpret_cast< volatile LONG * >( &inited ), 1, 0 ) != 0 )
-            {
-                return;
-            }
-            
-            objc       = LoadLibraryA( "objc.dll" );
-            foundation = LoadLibraryA( "Foundation.dll" );
-            
-            if( objc == nullptr || foundation == nullptr )
-            {
-                #ifdef _WIN64
-                common = nullptr;
-                #else
-                common = getenv( "COMMONPROGRAMFILES(x86)" );
-                #endif
-
-                if( common == nullptr )
+            std::call_once
+            (
+                once,
+                []
                 {
-                    common = getenv( "COMMONPROGRAMFILES" );
+                    #ifdef _WIN32
+                    
+                    char       * common;
+                    std::string  apple;
+                    HMODULE      objc;
+                    HMODULE      foundation;
+                    
+                    objc       = LoadLibraryA( "objc.dll" );
+                    foundation = LoadLibraryA( "Foundation.dll" );
+                    
+                    if( objc == nullptr || foundation == nullptr )
+                    {
+                        #ifdef _WIN64
+                        common = nullptr;
+                        #else
+                        common = getenv( "COMMONPROGRAMFILES(x86)" );
+                        #endif
+
+                        if( common == nullptr )
+                        {
+                            common = getenv( "COMMONPROGRAMFILES" );
+                        }
+                        
+                        apple = std::string( common ) + "\\Apple\\Apple Application Support";
+
+                        SetDllDirectoryA( apple.c_str() );
+
+                        objc       = LoadLibraryA( ( apple + "\\objc.dll" ).c_str() );
+                        foundation = LoadLibraryA( ( apple + "\\Foundation.dll" ).c_str() );
+                    }
+                    
+                    if( objc == nullptr || foundation == nullptr )
+                    {
+                        return;
+                    }
+                    
+                    Internal::objc_getClass                 = ( Class                         ( * )( const char * )                                       )GetProcAddress( objc, "objc_getClass" );
+                    Internal::objc_getMetaClass             = ( Class                         ( * )( const char * )                                       )GetProcAddress( objc, "objc_getMetaClass" );
+                    Internal::objc_getProtocol              = ( Protocol                  *   ( * )( const char * )                                       )GetProcAddress( objc, "objc_getProtocol" );
+                    Internal::objc_msgSend                  = ( void                          ( * )( void )                                               )GetProcAddress( objc, "objc_msgSend" );
+                    #ifndef __arm64__
+                    Internal::objc_msgSend_fpret            = ( void                          ( * )( void )                                               )GetProcAddress( objc, "objc_msgSend_fpret" );
+                    #endif
+                    Internal::objc_msgSendSuper             = ( void                          ( * )( void )                                               )GetProcAddress( objc, "objc_msgSendSuper" );
+                    Internal::objc_allocateClassPair        = ( Class                         ( * )( Class, const char *, size_t )                        )GetProcAddress( objc, "objc_allocateClassPair" );
+                    Internal::objc_registerClassPair        = ( void                          ( * )( Class )                                              )GetProcAddress( objc, "objc_registerClassPair" );
+                    Internal::objc_setAssociatedObject      = ( void                          ( * )( id, const void *, id, Internal::AssociationPolicy )  )GetProcAddress( objc, "objc_setAssociatedObject" );
+                    Internal::objc_getAssociatedObject      = ( id                            ( * )( id, const void * )                                   )GetProcAddress( objc, "objc_getAssociatedObject" );
+                    Internal::sel_registerName              = ( SEL                           ( * )( const char * )                                       )GetProcAddress( objc, "sel_registerName" );
+                    Internal::sel_getName                   = ( const char                  * ( * )( SEL )                                                )GetProcAddress( objc, "sel_getName" );
+                    Internal::object_getClass               = ( Class                         ( * )( id )                                                 )GetProcAddress( objc, "object_getClass" );
+                    Internal::method_getImplementation      = ( IMP                           ( * )( Method )                                             )GetProcAddress( objc, "method_getImplementation" );
+                    Internal::method_getName                = ( SEL                           ( * )( Method )                                             )GetProcAddress( objc, "method_getName" );
+                    Internal::method_getTypeEncoding        = ( const char                  * ( * )( Method )                                             )GetProcAddress( objc, "method_getTypeEncoding" );
+                    Internal::class_getSuperclass           = ( Class                         ( * )( Class )                                              )GetProcAddress( objc, "class_getSuperclass" );
+                    Internal::class_getName                 = ( const char                  * ( * )( Class )                                              )GetProcAddress( objc, "class_getName" );
+                    Internal::class_copyMethodList          = ( Method                      * ( * )( Class, unsigned int * )                              )GetProcAddress( objc, "class_copyMethodList" );
+                    Internal::class_addIvar                 = ( bool                          ( * )( Class, const char *, size_t, uint8_t, const char * ) )GetProcAddress( objc, "class_addIvar" );
+                    Internal::class_addMethod               = ( bool                          ( * )( Class, SEL, IMP, const char * )                      )GetProcAddress( objc, "class_addMethod" );
+                    Internal::class_addProtocol             = ( bool                          ( * )( Class, Protocol * )                                  )GetProcAddress( objc, "class_addProtocol" );
+                    Internal::class_getInstanceVariable     = ( Ivar                          ( * )( Class, const char * )                                )GetProcAddress( objc, "class_getInstanceVariable" );
+                    Internal::ivar_getOffset                = ( ptrdiff_t                     ( * )( Ivar )                                               )GetProcAddress( objc, "ivar_getOffset" );
+                    Internal::NSLogv                        = ( void                          ( * )( id, va_list )                                        )GetProcAddress( foundation, "NSLogv" );
+                    Internal::objc_setExceptionPreprocessor = ( objc_exception_preprocessor   ( * )( objc_exception_preprocessor )                        )GetProcAddress( objc, "objc_setExceptionPreprocessor" );
+
+                    #else
+                    
+                    Internal::objc_getClass                 = ::objc_getClass;
+                    Internal::objc_getMetaClass             = ::objc_getMetaClass;
+                    Internal::objc_getProtocol              = ::objc_getProtocol;
+                    Internal::objc_msgSend                  = ::objc_msgSend;
+                    #ifndef __arm64__
+                    Internal::objc_msgSend_fpret            = ::objc_msgSend_fpret;
+                    #endif
+                    Internal::objc_msgSendSuper             = ::objc_msgSendSuper;
+                    Internal::objc_allocateClassPair        = ::objc_allocateClassPair;
+                    Internal::objc_registerClassPair        = ::objc_registerClassPair;
+                    Internal::objc_setAssociatedObject      = ::objc_setAssociatedObject;
+                    Internal::objc_getAssociatedObject      = ::objc_getAssociatedObject;
+                    Internal::sel_registerName              = ::sel_registerName;
+                    Internal::sel_getName                   = ::sel_getName;
+                    Internal::object_getClass               = ::object_getClass;
+                    Internal::method_getImplementation      = ::method_getImplementation;
+                    Internal::method_getName                = ::method_getName;
+                    Internal::method_getTypeEncoding        = ::method_getTypeEncoding;
+                    Internal::class_getSuperclass           = ::class_getSuperclass;
+                    Internal::class_getName                 = ::class_getName;
+                    Internal::class_copyMethodList          = ::class_copyMethodList;
+                    Internal::class_addIvar                 = ::class_addIvar;
+                    Internal::class_addMethod               = ::class_addMethod;
+                    Internal::class_addProtocol             = ::class_addProtocol;
+                    Internal::class_getInstanceVariable     = ::class_getInstanceVariable;
+                    Internal::ivar_getOffset                = ::ivar_getOffset;
+                    Internal::NSLogv                        = ::NSLogv;
+                    Internal::objc_setExceptionPreprocessor = ::objc_setExceptionPreprocessor;
+                    
+                    #endif
+                    
+                    Internal::objc_setExceptionPreprocessor( OBJCXX_Exception_Preprocessor );
                 }
-                
-                apple = std::string( common ) + "\\Apple\\Apple Application Support";
-
-                SetDllDirectoryA( apple.c_str() );
-
-                objc       = LoadLibraryA( ( apple + "\\objc.dll" ).c_str() );
-                foundation = LoadLibraryA( ( apple + "\\Foundation.dll" ).c_str() );
-            }
-            
-            if( objc == nullptr || foundation == nullptr )
-            {
-                return;
-            }
-            
-            Internal::objc_getClass                 = ( Class                         ( * )( const char * )                                       )GetProcAddress( objc, "objc_getClass" );
-            Internal::objc_getMetaClass             = ( Class                         ( * )( const char * )                                       )GetProcAddress( objc, "objc_getMetaClass" );
-            Internal::objc_getProtocol              = ( Protocol                  *   ( * )( const char * )                                       )GetProcAddress( objc, "objc_getProtocol" );
-            Internal::objc_msgSend                  = ( void                          ( * )( void )                                               )GetProcAddress( objc, "objc_msgSend" );
-            #ifndef __arm64__
-            Internal::objc_msgSend_fpret            = ( void                          ( * )( void )                                               )GetProcAddress( objc, "objc_msgSend_fpret" );
-            #endif
-            Internal::objc_msgSendSuper             = ( void                          ( * )( void )                                               )GetProcAddress( objc, "objc_msgSendSuper" );
-            Internal::objc_allocateClassPair        = ( Class                         ( * )( Class, const char *, size_t )                        )GetProcAddress( objc, "objc_allocateClassPair" );
-            Internal::objc_registerClassPair        = ( void                          ( * )( Class )                                              )GetProcAddress( objc, "objc_registerClassPair" );
-            Internal::objc_setAssociatedObject      = ( void                          ( * )( id, const void *, id, Internal::AssociationPolicy )  )GetProcAddress( objc, "objc_setAssociatedObject" );
-            Internal::objc_getAssociatedObject      = ( id                            ( * )( id, const void * )                                   )GetProcAddress( objc, "objc_getAssociatedObject" );
-            Internal::sel_registerName              = ( SEL                           ( * )( const char * )                                       )GetProcAddress( objc, "sel_registerName" );
-            Internal::sel_getName                   = ( const char                  * ( * )( SEL )                                                )GetProcAddress( objc, "sel_getName" );
-            Internal::object_getClass               = ( Class                         ( * )( id )                                                 )GetProcAddress( objc, "object_getClass" );
-            Internal::method_getImplementation      = ( IMP                           ( * )( Method )                                             )GetProcAddress( objc, "method_getImplementation" );
-            Internal::method_getName                = ( SEL                           ( * )( Method )                                             )GetProcAddress( objc, "method_getName" );
-            Internal::method_getTypeEncoding        = ( const char                  * ( * )( Method )                                             )GetProcAddress( objc, "method_getTypeEncoding" );
-            Internal::class_getSuperclass           = ( Class                         ( * )( Class )                                              )GetProcAddress( objc, "class_getSuperclass" );
-            Internal::class_getName                 = ( const char                  * ( * )( Class )                                              )GetProcAddress( objc, "class_getName" );
-            Internal::class_copyMethodList          = ( Method                      * ( * )( Class, unsigned int * )                              )GetProcAddress( objc, "class_copyMethodList" );
-            Internal::class_addIvar                 = ( bool                          ( * )( Class, const char *, size_t, uint8_t, const char * ) )GetProcAddress( objc, "class_addIvar" );
-            Internal::class_addMethod               = ( bool                          ( * )( Class, SEL, IMP, const char * )                      )GetProcAddress( objc, "class_addMethod" );
-            Internal::class_addProtocol             = ( bool                          ( * )( Class, Protocol * )                                  )GetProcAddress( objc, "class_addProtocol" );
-            Internal::class_getInstanceVariable     = ( Ivar                          ( * )( Class, const char * )                                )GetProcAddress( objc, "class_getInstanceVariable" );
-            Internal::ivar_getOffset                = ( ptrdiff_t                     ( * )( Ivar )                                               )GetProcAddress( objc, "ivar_getOffset" );
-            Internal::NSLogv                        = ( void                          ( * )( id, va_list )                                        )GetProcAddress( foundation, "NSLogv" );
-            Internal::objc_setExceptionPreprocessor = ( objc_exception_preprocessor   ( * )( objc_exception_preprocessor )                        )GetProcAddress( objc, "objc_setExceptionPreprocessor" );
-
-            #else
-            
-            if( OSAtomicCompareAndSwap32( 0, 1, &inited ) == false )
-            {
-                return;
-            }
-            
-            Internal::objc_getClass                 = ::objc_getClass;
-            Internal::objc_getMetaClass             = ::objc_getMetaClass;
-            Internal::objc_getProtocol              = ::objc_getProtocol;
-            Internal::objc_msgSend                  = ::objc_msgSend;
-            #ifndef __arm64__
-            Internal::objc_msgSend_fpret            = ::objc_msgSend_fpret;
-            #endif
-            Internal::objc_msgSendSuper             = ::objc_msgSendSuper;
-            Internal::objc_allocateClassPair        = ::objc_allocateClassPair;
-            Internal::objc_registerClassPair        = ::objc_registerClassPair;
-            Internal::objc_setAssociatedObject      = ::objc_setAssociatedObject;
-            Internal::objc_getAssociatedObject      = ::objc_getAssociatedObject;
-            Internal::sel_registerName              = ::sel_registerName;
-            Internal::sel_getName                   = ::sel_getName;
-            Internal::object_getClass               = ::object_getClass;
-            Internal::method_getImplementation      = ::method_getImplementation;
-            Internal::method_getName                = ::method_getName;
-            Internal::method_getTypeEncoding        = ::method_getTypeEncoding;
-            Internal::class_getSuperclass           = ::class_getSuperclass;
-            Internal::class_getName                 = ::class_getName;
-            Internal::class_copyMethodList          = ::class_copyMethodList;
-            Internal::class_addIvar                 = ::class_addIvar;
-            Internal::class_addMethod               = ::class_addMethod;
-            Internal::class_addProtocol             = ::class_addProtocol;
-            Internal::class_getInstanceVariable     = ::class_getInstanceVariable;
-            Internal::ivar_getOffset                = ::ivar_getOffset;
-            Internal::NSLogv                        = ::NSLogv;
-            Internal::objc_setExceptionPreprocessor = ::objc_setExceptionPreprocessor;
-            
-            #endif
-            
-            Internal::objc_setExceptionPreprocessor( OBJCXX_Exception_Preprocessor );
+            );
         }
     }
 }
